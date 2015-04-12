437c93251680fe51a4eb4d1ba88b73d2749c3fb9
Henry J Schmale <hschmale16@gmail.com>
2015-04-12 11:49:36 -0400

    Improved Project Setup Script

--- cpp_project_setup.sh
+++ cpp_project_setup.sh
@@ -10,5 +10,16 @@
 # get the makefile
 wget https://raw.githubusercontent.com/HSchmale16/GenericMakefile/master/cpp/Makefile -O makefile
 
-# get the gitignore
-wget https://raw.githubusercontent.com/github/gitignore/master/C%2B%2B.gitignore
+# get the gitignore and set it up to work with the makefile
+wget https://raw.githubusercontent.com/github/gitignore/master/C%2B%2B.gitignore -o .gitignore
+echo "*.d"   >> .gitignore
+echo "bin/*" >> .gitignore
+
+# Create necessary files
+touch readme.md
+touch main.cpp
+
+# Say end message for what the user still has to do"
+echo "Project Creation Complete!"
+echo "Now edit the makefile to include the libraries you need for your project and title" \
+     " your project's binary"
