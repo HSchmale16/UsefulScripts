0f878eee1ed861ef340cc13cf23f5af566d0f795
Henry J Schmale <hschmale16@gmail.com>
2015-04-24 11:25:54 -0400

    Improved project setup script with args for additional config of project

--- quick-cpp-setup.sh
+++ quick-cpp-setup.sh
@@ -6,18 +6,33 @@
 # 
 # Henry J Schmale
 # April 12, 2015
+#
+# Accepts the following parameters
+# $1 - project name
+# $2 - output binary name
 
 # get the makefile
 wget https://raw.githubusercontent.com/HSchmale16/GenericMakefile/master/cpp/Makefile -O makefile
+# edit the makefile with perl
+perl -pi -e 's/hello/$2/g' makefile
 
 # get the gitignore and set it up to work with the makefile
 wget https://raw.githubusercontent.com/github/gitignore/master/C%2B%2B.gitignore -o .gitignore
 echo "*.d"   >> .gitignore
 echo "bin/*" >> .gitignore
 
-# Create necessary files
+# Create necessary files & put stuff in them
 touch readme.md
+if [ -z $1 ] ; then
+    echo "# $1" > readme.md
+fi
+
 touch main.cpp
+echo '/**\file main.cpp' >> main.cpp
+echo " * \\author $(git config user.name)" >> main.cpp
+echo " * \\date $(date)" >> main.cpp
+echo " */" >> main.cpp
+echo "\n int main(int argc, char*argv){\n\n}"
 
 # Say end message for what the user still has to do"
 echo "Project Creation Complete!"
