74e5f2a3c2c6e9c5db80c3185dcae7e7c21f1506
Henry J Schmale <hschmale16@gmail.com>
2015-04-24 11:57:36 -0400

    Output the skeleton of a main.cpp file

--- quick-cpp-setup.sh
+++ quick-cpp-setup.sh
@@ -11,30 +11,53 @@
 # $1 - project name
 # $2 - output binary name
 
+function argsList(){
+    echo "Usage: $0 <project-name> <output-binary-name>"
+}
+
+# check for args
+if [ -z $1 ] ; then
+    echo missing arg 1
+    argsList
+    exit
+fi
+if [ -z $2 ] ; then
+    echo missing arg 2 - output binary name
+    argsList
+    exit
+fi
+
+# =====================================
+# End the Args Parsing
+# Begin the meat of the script
+# =====================================
+
 # get the makefile
-wget https://raw.githubusercontent.com/HSchmale16/GenericMakefile/master/cpp/Makefile -O makefile
+wget https://raw.githubusercontent.com/HSchmale16/GenericMakefile/master/cpp/Makefile -O makefile > /dev/null
 # edit the makefile with perl
 perl -pi -e 's/hello/$2/g' makefile
 
 # get the gitignore and set it up to work with the makefile
-wget https://raw.githubusercontent.com/github/gitignore/master/C%2B%2B.gitignore -o .gitignore
+wget https://raw.githubusercontent.com/github/gitignore/master/C%2B%2B.gitignore -O .gitignore > /dev/null
 echo "*.d"   >> .gitignore
 echo "bin/*" >> .gitignore
+echo $1 >> .gitignore
 
 # Create necessary files & put stuff in them
 touch readme.md
 if [ -z $1 ] ; then
-    echo "# $1" > readme.md
+    echo \# $1 > readme.md
 fi
 
 touch main.cpp
 echo '/**\file main.cpp' >> main.cpp
-echo " * \\author $(git config user.name)" >> main.cpp
-echo " * \\date $(date)" >> main.cpp
-echo " */" >> main.cpp
-echo "\n int main(int argc, char*argv){\n\n}"
-
-# Say end message for what the user still has to do"
+echo ''\* \\author $(git config user.name) >> main.cpp
+echo ''\* \\date $(date) >> main.cpp
+echo ''*/ >> main.cpp
+echo >> main.cpp
+echo int main\(int argc, char*argv\)\{ >> main.cpp
+echo >> main.cpp
+echo \} >> main.cpp
+# Say end message for what the user still has to do
 echo "Project Creation Complete!"
-echo "Now edit the makefile to include the libraries you need for your project and title" \
-     " your project's binary"
+echo Now edit the makefile to include the libraries you need for your project
