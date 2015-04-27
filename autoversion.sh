00a4aaab8129951af962b3c77ae15bb8427517c6
Henry J Schmale <hschmale16@gmail.com>
2015-04-27 19:07:45 -0400

    Autoversion Script

---
+++ autoversion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Autoversion Script
+# A script to make an autoversion header
+#
+# usage:
+#   version.h: $(SRC)
+#       @./autoversion.sh
+
+rev=$(git rev-list HEAD --count)
+myd=$(date +%m/%d/%Y)
+vh=version.h
+
+echo "#ifndef VERSION_H_INC" > $vh
+echo "#define VERSION_H_INC" >> $vh
+echo "const int REVSION       = $rev;" >> $vh
+echo "const char BUILD_DATE[] = \"$myd\";" >> $vh
+echo "#endif // VERSION_H_INC" >> $vh
