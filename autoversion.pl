a224808a22e3fc1e20dbd467480fabd7772f6afe
Henry J Schmale <hschmale16@gmail.com>
2015-04-28 11:51:13 -0400

    Update autoversion.pl

--- autoversion.pl
+++ autoversion.pl
@@ -1,17 +1,23 @@
 #!/usr/bin/perl
+# Henry J Schmale
+# April 28, 2015
+#
 # Autoversion Script
-# A script to make an autoversion header
+# A script to make an autoversion header for C/C++ projects
 #
 # usage:
 #   version.h: $(SRC)
-#       @./autoversion.sh
+#       @./autoversion.pl
 
-rev=$(git rev-list HEAD --count)
-myd=$(date +%m/%d/%Y)
-vh=version.h
+$rev = `git rev-list HEAD --count`;
+$myd = `date +%m/%d/%Y`;
+$vh  = "version.h";
 
-echo "#ifndef VERSION_H_INC" > $vh
-echo "#define VERSION_H_INC" >> $vh
-echo "const int REVSION       = $rev;" >> $vh
-echo "const char BUILD_DATE[] = \"$myd\";" >> $vh
-echo "#endif // VERSION_H_INC" >> $vh
+# Prepare the contents
+$con =
+    "#ifndef VERSION_H_INC\n".
+    "#define VERSION_H_INC\n".
+    "const int REVSION = $rev;\n".
+    "const char BUILD_DATE[] = \"$myd;\"\n".
+    "#endif // VERSION_H_INC\n";
+    
