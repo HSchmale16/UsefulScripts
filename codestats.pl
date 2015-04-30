6a54249aa146f6d51e290d9f503a02b0564bd5a4
Henry J Schmale <hschmale16@gmail.com>
2015-04-30 17:29:33 -0400

    Create codestats.pl
    Code statistics script

---
+++ codestats.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+# codestats.pl
+# A perl script to calculate the number of lines of code in a project
+# and what type of lines those code are, such as comments or code, or
+# whatnot. This script is specific to C/C++, but should be able to be
+# adapted to other languages.
+#
+# Henry J Schmale
+# April 30, 2015
