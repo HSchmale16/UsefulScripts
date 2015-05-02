b7f88d89895a023a18f29bc9e849a92d36416e5c
Henry J Schmale <hschmale16@gmail.com>
2015-05-02 18:04:57 -0400

    Reads files, but doesn't parse lines yet

--- codestats.pl
+++ codestats.pl
@@ -10,18 +10,35 @@
 
 use strict;
 use warnings;
-use File::Find;
+
+my @srcext = ("*.cpp", "*.c", "*.h", "*.hpp");
 
 my $commLines = 0; # Lines that start with a comment symbol
+my $bothLines = 0; # Lines that have a comment and code
 my $newLines  = 0; # Lines that are just whitespace
 my $codeLines = 0; # Lines of code - lines that don't fit in another space
 
-
-sub processFiles{
-    # Process all of the files   
+my $files;
+for($a = 0; $a < scalar(@srcext); $a++){
+    $files .= `find . -name "$srcext[$a]"`;
 }
+my @inputs = split("\n", $files);
+for($a = 0; $a < scalar(@inputs); $a++){
+    print "Reading $inputs[$a]\n";
+    countLines($inputs[$a]);
+}
+# print out the results
+printf("Comment Lines: %d\n", $commLines);
+printf("Blank Lines  : %d\n", $newLines);
 
+# Count the lines in the given file
+# The first param is the file to open for counting
 sub countLines{
-    my ($file) = @_;
-    open(FILE, "<$file") or die "Couldn't open file: $!\n";
+    my ($srcfile) = @_;
+    open(FILE, "<$srcfile") or die "Couldn't open file: $!\n";
+    my @lines = <FILE>;
+    for($b = 0; $b < scalar(@lines); $b++){
+        
+    }
+    close FILE;
 }
