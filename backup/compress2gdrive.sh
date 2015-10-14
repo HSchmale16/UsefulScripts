21e024b7506727bb917f3863d41808e35badcfe2
Henry J Schmale <hschmale16@gmail.com>
2015-10-14 15:01:44 -0400

    Finished the file compress and upload to gdrive script

--- backup/compress2gdrive.sh
+++ backup/compress2gdrive.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
-# Accepts a single arg then uploads to google drive. 
+# Henry J Schmale
+# October 14, 2015
+# 
+# Accepts a single arg then uploads to google drive into the backup
+# directory.
 # 
 # ./compress2gdrive.sh <FILE>
+ 
 
 if [ ! -f $1 ] ; then 
-    echo $1 does not exist
+    echo $1 does not exist exit
+    exit
 fi
+
+TMPFILE=/tmp/$(basename $1)
+BZIPFILE=${TMPFILE}.bz2
+REMOTEDIR="backup"
+
+cp "$1" "$TMPFILE"
+bzip2 $TMPFILE
+
+# Prepare to upload
+FOLDER_INFO=$(drive list | grep "$REMOTEDIR")
+
+# Create backup folder because it does not exists yet and get the id of the
+# parent to place file in
+if [ -z "$FOLDER_INFO" ] ; then
+    PARENT_ID=$(drive folder -t "$REMOTEDIR")
+    PARENT_ID=$(echo $PARENT_ID | awk '{print $2}')
+else
+    PARENT_ID=$(echo "$FOLDER_INFO" | awk '{print $1}' | head -1)
+fi
+
+# Upload the file
+drive upload -p "$PARENT_ID" -f "$BZIPFILE"
