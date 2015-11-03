a672b625b1fab40e1aae6850a651931628d5ca92
Henry J Schmale <hschmale16@gmail.com>
2015-11-03 16:15:29 -0500

    Improved the git2gdrive script
    It will now check if you have the drive utility in your path. If you do not
    it will refuse to run. This was an issue when setting it up as a cronjob,
    because `drive` was not in my path originally. I also improved the
    documentation within the script. I also removed the directory manipulations
    from the script, because that is not required anymore because I discovered
    `git archive`, and the script has been using that for a while now.

--- backup/git2gdrive.sh
+++ backup/git2gdrive.sh
@@ -20,13 +20,27 @@
 LOCALDIR=$1
 REMOTEDIR="backup"
 
+# Check that required programs are installed
+# Please see the note in the preamble(at the top) for the required
+# programs.
+if ! type -p drive 2>&1 /dev/null
+then
+    echo \`drive\` is not in your path, add it to your path to use this \
+        script.
+    exit
+fi
+
+
+
 # Input Verification
 # This stuff checks the arguements to the backup script.
-if [ -z "$LOCALDIR" ] ; then
+if [ -z "$LOCALDIR" ]
+then
     echo "You need to specify the directory to backup"
     exit
 else
-    if [ ! -d "$LOCALDIR" ] ; then
+    if [ ! -d "$LOCALDIR" ]
+    then
         echo "$LOCALDIR is not a directory"
         exit
     fi
@@ -36,31 +50,23 @@ fi
 DATE=$(date +%Y-%m-%d)
 TARNAME="/tmp/$(basename $LOCALDIR)$DATE.tbz"
 
-# Reduce the size of the directoy by doing some stuff with git and make
-# I don't want to upload binaries to google drive, because they can get big
-# very quick.
 cd $LOCALDIR
 echo Enter $(pwd)
-echo clean the build stuff
-if [ -f "makefile" ] ; then
-    make clean
-fi
-if [ -d "./.git" ] ; then
-    echo Is Git Repo, Reducing Repo size
-    git gc --aggressive --quiet
-fi
 
 # Generate the archive
-git archive --format=tar --prefix=$(basename $LOCALDIR)/ HEAD | bzip2 \
-    > $TARNAME
+git archive --format=tar --prefix=$(basename $LOCALDIR)/ HEAD \
+    | bzip2 > $TARNAME
 
 # Prepare to upload
 # Get the remote directory id to place the back up in.
 FOLDER_INFO=$(drive list | grep "$REMOTEDIR")
+echo Got Folder Info
 
 # Create backup folder if it does not exists yet and get the id of the
 # parent to place file in
-if [ -z "$FOLDER_INFO" ] ; then
+if [ -z "$FOLDER_INFO" ]
+then
+    echo Folder does not exist creating the folder
     PARENT_ID=$(drive folder -t "$REMOTEDIR")
     PARENT_ID=$(echo $PARENT_ID | awk '{print $2}')
 else
