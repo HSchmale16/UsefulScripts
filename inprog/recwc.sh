5bfd2ea81857327277ae9021668130f867534e81
Henry J Schmale <hschmale16@gmail.com>
2015-11-07 19:35:37 -0500

    Worked some more on a dumb script

--- inprog/recwc.sh
+++ inprog/recwc.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+function total-wc {
+    wc $@ | tail -1
+}
+
 LATEX_FILES=$(find . -name '*.tex')
 TXT_FILES=$(find . -name '*.txt')
 MD_FILES=$(find . -name '*.md')
+
+LATEX_WC=$(total-wc ${LATEX_FILES})
+TXT_WC=$(total-wc ${TXT_FILES})
+MD_WC=$(total-wc ${MD_FILES})
+
+echo $LATEX_WC
+
+
