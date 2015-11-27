81016c7d6f1a2f893bc113b7a772fb8cb887bc6b
Henry J Schmale <hschmale16@gmail.com>
2015-11-27 13:08:51 -0500

    Keep working on dir sync

--- backup/gdrive-dirsync.sh
+++ backup/gdrive-dirsync.sh
@@ -16,28 +16,44 @@
 # $0 <push|pull|help> <local-dir> <gdrive-dir>
 
 function printScriptUsage() {
-    echo USAGE:
-    echo $0 '<push|pull|help> <local-dir> <gdrive-dirname>'
+    echo "USAGE: "
+    echo "$0 <push|pull|help> <local-dir> <gdrive-dirname>"
 }
 
+# check that the script has it's dependicies that it needs
+function checkDepends() {
+    # check for required programs. Check the preamble for the required tools
+    if type -p drive 2>&1 /dev/null
+    then
+        echo "You need the pramussan/gdrive tool to run this script"
+        exit
+    fi 
+}
+
+function dir-sync-push() {
+}
+
+function dir-sync-pull() {
+
+}
 
 # Check and Parse args
-CMD=$1
+SCRIPTCMD=$1
 LOCAL_DIR=$2
 GDRIVE_DIR=$3
 
-case $CMD in
-    push)
-        ;;
-    pull)
+case $SCRIPTCMD in
+    push|pull)
+        checkDepends
+        GDRIVE_LISTING=$(drive list)
         ;;
     help)
-        printScriptUsage
+        printScriptUsage $1
         exit
         ;;
     *)
         echo Invalid Command
-        echo For help run: "$0 help"
+        echo "For help run: $0 help [cmd]"
         exit
         ;;
 esac
