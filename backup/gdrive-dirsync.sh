381364132f4e3f985928353c10fbcb8dc35e4a98
Henry J Schmale <hschmale16@gmail.com>
2015-11-29 14:02:15 -0500

    Work on the sync script some more

--- backup/gdrive-dirsync.sh
+++ backup/gdrive-dirsync.sh
@@ -7,21 +7,24 @@
 # synchronise my creeper world 3 game on my computers, and various other
 # directories.
 # 
-# It should be noted that this script will not sync with the root directory
+# It should be noted that this script should be used to sync giant
+# directories.
 #
 # This script requires the pramussan/gdrive tool in your path, and it should
 # be named or aliased to `drive`.
+#
 
 # print the script usage message
 function printScriptUsage() {
     echo "USAGE: "
     echo "$0 <push|pull|help> ..."
     echo
-    echo "push - push a directory to a google drive directory"
+    echo "push - push a directory to a google drive directory."
+    echo "       This will define the local directory as the master"
     echo "    $0 push <local-dir> <gdrive-dir>"
     echo "pull - pulls the changes from google drive to a local directory"
     echo "    $0 pull <local-dir> <gdrive-dir>"
-    echo "help - display this help"
+    echo "help - display this help message"
 }
 
 # check that the script has it's dependicies that it needs
@@ -31,7 +34,32 @@ function checkDepends() {
     then
         echo "You need the pramussan/gdrive tool to run this script"
         exit
-    fi 
+    fi
+    # check args
+    if [ -z "$LOCAL_DIR" ]
+    then
+        echo "LOCAL_DIR is not specified"
+        exit
+    else
+        if [ ! -d "$LOCAL_DIR" ]
+        then
+            echo "Local Dir Invalid"
+            exit
+        fi
+    fi
+    if [ -z "$GDRIVE_DIR" ]
+    then
+        echo "GDRIVE_DIR Invalid"
+        exit
+    fi
+}
+
+# Builds a listing of a local directory, places it into a file, and returns
+# the name of the file.
+function buildLocalList() { 
+    TEMPFILE=$(mktemp)
+    find "$LOCAL_DIR" | sort > $TEMPFILE
+    echo $TEMPFILE
 }
 
 function dir-sync-push() {
@@ -50,7 +78,8 @@ GDRIVE_DIR=$3
 case $SCRIPTCMD in
     push|pull)
         checkDepends
-        GDRIVE_LISTING=$(drive list)
+        LOCALFILELIST=$(buildLocalList)
+        echo $LOCALFILELIST
         ;;
     help)
         printScriptUsage $1
